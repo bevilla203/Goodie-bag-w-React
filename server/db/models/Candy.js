d8bf851745ba44274e5d32fef65312f37946ebf2
bvilla203 <ben.tan.villafranca@gmail.com>
2023-02-07 18:22:40 -0500

    finished candies model

--- server/db/models/Candy.js
+++ server/db/models/Candy.js
@@ -1,6 +1,32 @@
-const Sequelize = require('sequelize');
-const db = require('../database');
+const Sequelize = require("sequelize");
+const db = require("../database");
 
-module.exports = db.define('candy', {
-  // define your model here!
+module.exports = db.define("candy", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    validate: {
+      isInt: true,
+      max: 10
+    }
+  },
+  imageUrl: {
+    defaultValue: '',
+    validate: {
+      isUrl: true
+    }
+  }
 });
